21bbc5210d5f23a1c52afb93e267e6f877ab62b9
Dave Coate <coateds@outlook.com>
2018-10-22 19:01:24 -0700

    shoot aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,19 +17,18 @@ def run_game():
     # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
     bullets = Group()
-    # This line not needed anymore
-    # alien = Alien(ai_settings, screen)
     aliens = Group()
 
     # Create the fleet of aliens.
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
 
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, ship, aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
